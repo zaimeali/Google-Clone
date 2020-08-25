2e41d9ed643e88186a92a578a5b9f306c7bff8a6
zaimeali <zaimeali1997@gmail.com>
2020-08-25 19:48:25 +0500

    working on search page response

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -7,19 +7,33 @@ import { useStateValue } from '../StateProvider'
 import './SearchPage.css'
 
 // Custom Hook
-import useGoogleSearch from '../useGoogleSearch'
+// import useGoogleSearch from '../useGoogleSearch'
+
+// Local Call Response
+import Response from '../response';
+
+// React Router
+import { Link } from 'react-router-dom'
 
 function SearchPage() {
 
     const [{ term }, dispatch] = useStateValue();
 
-    const { data } = useGoogleSearch(term)
+    // LIVE API CALL
+    // const { data } = useGoogleSearch(term)
+
+    const data = Response
 
     console.log(data)
     return (
         <div className="searchPage">
             <div className="searchPage__header">
-                <h1>{ term }</h1>
+                <Link to="/">
+                    <img 
+                        src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+                        alt="google logo"
+                    />
+                </Link>
             </div>
             <div className="searchPage__results">
 
